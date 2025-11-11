d577db3f98508172cad98ebcbb7a6fd43a35ef80
kate-shih <166676277+kate-shih@users.noreply.github.com>
2025-11-11 13:15:43 -0600

    added data viz

--- script.js
+++ script.js
@@ -1,54 +1,70 @@
+let angle;
 
-
-async function loadData(){
-	let data = await d3.csv("catData.csv", d3.autoType); 
-	let columns = data.columns;
-	console.log(columns)
-
-	data = d3.filter(data, (v) => {
-		return v.size <= 20 
-	})
-	console.log(data);
-
-	data = d3.sort(data, (a, b) => {
-		return d3.descending(a.size, b.size)
-	})
-	console.log(data);
-
-	let mean = d3.mean(data, (d) => {
-		return d.size; 
-	});
-	console.log(mean)
-
-	let table = document.createElement("table")
-
-	let header = "<tr>"
-	columns.forEach((v) => {
-		header += "<th>" + v + "</th>";
-	})
-	header += "</tr>";
-
-	let rows = "";
-	data.forEach((v) => {
-		rows += "<tr>"
-
-		rows += "<td>" + v.name + "</td>";
-		rows += "<td>" + v.size + "</td>";
-		rows += "<td>" + v["favorite food"] + "</td>";
-		rows += "<td>" + v.owner + "</td>";
-
-		rows += "</tr>"
-	})
-	console.log(rows)
-
-	let tableHTML = header + rows
-	table.innerHTML = tableHTML;
-
-	let body = document.querySelector("body");
-	body.appendChild(table)
+function setup() {
+  createCanvas(710, 400);
+  colorMode(HSB);
+  angleMode(DEGREES);
 }
 
+function draw() {
+  background(0);
 
-window.onload = function() {
-	loadData();
+  // Calculate the angle based on the mouse position, maximum 90 degrees
+  angle = (mouseX / width) * 90;
+  angle = min(angle, 90);
+
+  // Start the tree from the bottom of the screen
+  translate(width / 2, height);
+
+  // Draw a line 120 pixels
+  stroke(0, 255, 255);
+  line(0, 0, 0, -120);
+
+  // Move to the end of that line
+  translate(0, -120);
+
+  // Start the recursive branching
+  branch(120, 0);
+
+  describe(
+    'A tree drawn by recursively drawing branches, with angle determined by the user mouse position.'
+  );
+}
+
+function branch(h, level) {
+  // Set the hue based on the recursion level
+  stroke(level * 25, 255, 255);
+
+  // Each branch will be 2/3 the size of the previous one
+  h *= 0.66;
+
+  // Draw if our branch length > 2, otherwise stop the recursion
+  if (h > 2) {
+    // Draw the right branch
+    // Save the current coordinate system
+    push();
+
+    // Rotate by angle
+    rotate(angle);
+
+    // Draw the branch
+    line(0, 0, 0, -h);
+
+    // Move to the end of the branch
+    translate(0, -h);
+
+    // Call branch() recursively
+    branch(h, level + 1);
+
+    // Restore the saved coordinate system
+    pop();
+
+    // Draw the left branch
+    push();
+    rotate(-angle);
+    line(0, 0, 0, -h);
+    translate(0, -h);
+    branch(h, level + 1);
+    pop();
+  }
 }
